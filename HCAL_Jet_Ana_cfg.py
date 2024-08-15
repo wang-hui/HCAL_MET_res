3aeaa272970bf6f401fdd0726e78e7cbdf0e0af5
Hui <66718889@qq.com>
2024-08-15 06:13:33 +0200

    change to run3

--- HCAL_Jet_Ana_cfg.py
+++ HCAL_Jet_Ana_cfg.py
@@ -1,7 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 import sys
-from Configuration.StandardSequences.Eras import eras
-process = cms.Process("Test", eras.Run2_2018)
+from Configuration.Eras.Era_Run3_2023_cff import Run3_2023
+
+process = cms.Process('Test',Run3_2023)
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 10000
@@ -11,26 +12,25 @@ process.load("Configuration.StandardSequences.GeometryDB_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 
 # global tag
-globalTag = '106X_upgrade2018_realistic_v11_L1v1'
-process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = globalTag
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '130X_mcRun3_2023_realistic_postBPix_v2', '')
 
 process.options = cms.untracked.PSet(
   numberOfThreads=cms.untracked.uint32(1)
 )
 
-f = open(sys.argv[2], "r")
+f = open(sys.argv[1], "r")
 my_list = f.readlines()
 f.close()
 
-if len(sys.argv) == 4:
-    nFile = int(sys.argv[3])
+if len(sys.argv) == 3:
+    nFile = int(sys.argv[2])
     my_list = my_list[0:nFile]
 
-OutputFile = sys.argv[2].split("/")[-1]
+OutputFile = sys.argv[1].split("/")[-1]
 OutputFile = OutputFile.split(".")[0]
 OutputFile = OutputFile + "_TTree.root"
-#OutputFile = "test_plots.root"
 
 # source
 process.source = cms.Source(
@@ -46,6 +46,9 @@ process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
 )
 process.TFileService = cms.Service("TFileService", fileName = cms.string(OutputFile) )
+
+#TightCut requires each event has two CaloJet match to GenJet
+#Set to False if using single particle gun
 process.myAna = cms.EDAnalyzer("HCAL_Jet_Ana", TightCut = cms.untracked.bool(False))
 
 process.path = cms.Path(process.myAna)
